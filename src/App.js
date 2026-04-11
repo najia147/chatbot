bbd249ae5741ad99af09579756115bdacdf19b5e
Najia <anajia231@gmail.com>
2026-04-11 20:56:49 +0430

    refactor: update App component structure and improve message handling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import ChatInput from "./ChatInput";
 import ChatMessage from "./ChatMessage";
 
-function App() {
-  let [chatMessages, setChatMessages] = useState([
-    {
-      message: "Hello chatbot",
-      sender: "user",
-      id: 1,
-    },
-    {
-      message: "Hi, how can I assist you today?",
-      sender: "robot",
-      id: 2,
-    },
-    {
-      message: "Can you give me the date of today?",
-      sender: "user",
-      id: 3,
-    },
-  ]);
+  function App() {
+  
+    const [ChatMessages,setChatMessages] = useState ([
+      {
+        message: 'Hello chatbot.',
+        sender: 'user',
+        id: 'id1'
+      },
+      {
+        message: 'Hi, how can I help you?',
+        sender: 'robot',
+        id: 'id2'
+      },
+      { 
+        message: 'Can you get me today date?',
+        sender: 'user',
+        id: 'id3'
+      }]);  
+    
+    const chatMessagesRef = useRef(null);
 
-  return (
-    <>
-      <ChatInput
-        chatMessages={chatMessages}
-        setChatMessages={setChatMessages}
-      />
-      {chatMessages.map((chatMessage) => {
-        return (
-          <ChatMessage
-            message={chatMessage.message}
-            sender={chatMessage.sender}
-            key={chatMessage.id}
-          />
-        );
-      })}
-    </>
-  );
-}
+    useEffect(() => {
+      const containerElem = chatMessagesRef.current;
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }, [chatMessagesRef]);
+
+
+    return (
+      <div
+       style={{
+        maxWidth: '600px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: '0px',
+        marginBottom: '0px',
+
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column'
+       }}
+       ref={chatMessagesRef}
+      >
+        <div style={{
+          flexGrow: '1',
+          marginTop: '20px',
+          overflow: 'scroll',
+          scrollbarWidth: 'none'
+        }}>
+          {
+            ChatMessages.map(({ message, sender, id }) => {
+              return (
+                <ChatMessage 
+                  key={id} 
+                  message={message} 
+                  sender={sender} 
+                />
+              )
+            })
+          }
+        </div>
+        <ChatInput 
+          ChatMessages={ChatMessages} 
+          setChatMessages={setChatMessages}
+          style={{
+            display: 'flex',
+            marginBottom: '60px'
+          }}
+       />
+      </div>
+    );
+  }
 
 export default App;
